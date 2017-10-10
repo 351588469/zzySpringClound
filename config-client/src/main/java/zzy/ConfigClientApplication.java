7a5ea9f3656d3c980674af190f12968a3f38ddee
zzy <zhaozhengyang@x9v.com.cn>
2017-10-10 16:51:05 +0800

    config加入eureka

--- config-client/src/main/java/zzy/ConfigClientApplication.java
+++ config-client/src/main/java/zzy/ConfigClientApplication.java
@@ -3,11 +3,15 @@ package zzy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@RefreshScope//重新初始化并载入新的配置内容
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
